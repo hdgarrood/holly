6232e63dc3352931c417831c866c7f8b2b6113d3
Harry Garrood <harry@garrood.me>
2015-07-10 18:49:53 +0100

    It works!!!

--- src/Main.purs
+++ src/Main.purs
@@ -1,22 +1,32 @@
 module Main where
 
 import Prelude
-import Control.Bind ((>=>))
-import Data.Foldable (for_)
+import Control.Bind
+import Data.Foldable
 import Data.Maybe
+import Data.Either
+
 import qualified Data.Array as A
+import qualified Data.Array.Unsafe as AU
 import qualified Data.String as S
+
+import Data.Foreign.Class (IsForeign, read, readProp)
 import Control.Monad.Trans (lift)
 import Control.Monad.Eff.Console (CONSOLE())
+import Control.Monad.Eff.Exception (error)
+import Control.Monad.Error.Class (throwError)
 import Control.Monad.Aff
-import Network.HTTP.Affjax
+import qualified Network.HTTP.Affjax as Affjax
+import qualified Network.HTTP.Affjax.Response as Affjax
+import qualified Network.HTTP.MimeType.Common as Mime
+import qualified Network.HTTP.RequestHeader as Header
 import Node.IRC
 
-type Effects e = (ajax :: AJAX, console :: CONSOLE | e)
+type Effects e = (ajax :: Affjax.AJAX, console :: CONSOLE | e)
 
 main = launchAff $ do
-  let chan = Channel "#purescript"
-  connect (Host "irc.freenode.net") (Nick "holly") chan $ do
+  let chan = Channel "#purescript-bot-testing"
+  connect (Host "irc.freenode.net") (Nick "holly_") chan $ do
     sayChannel chan (MessageText "Hello, world")
     onChannelMessage chan \event -> do
       let mreq = parseRequest $ runMessageText event.text
@@ -42,8 +52,28 @@ performRequest :: forall e. Request -> Aff (Effects e) String
 performRequest req = case req of
   PursuitQuery query -> performPursuitQuery query
 
+pursuitBaseURL :: String
+pursuitBaseURL = "http://localhost:3000"
+
 performPursuitQuery :: forall e. String -> Aff (Effects e) String
-performPursuitQuery = map _.response <<< get <<< ("/search?q=" <>)
+performPursuitQuery =
+  makeUrl
+    >>> getJSON
+    -- >>> assertOk200
+    >>> map _.response
+    >=> readForeign
+    >>> map (AU.head >>> renderResult)
+  where
+  makeUrl q = pursuitBaseURL <> "/search?q=" <> q
+
+  readForeign = either (throwError <<< error <<< show) return <<< read
+
+getJSON :: forall e a. (Affjax.Respondable a) => String -> Affjax.Affjax e a
+getJSON url =
+  Affjax.affjax $ Affjax.defaultRequest
+    { url = url
+    , headers = [Header.Accept Mime.applicationJSON]
+    }
 
 whenJust :: forall m a. (Applicative m) => Maybe a -> (a -> m Unit) -> m Unit
 whenJust = for_
@@ -53,3 +83,18 @@ stripPrefix prefix str =
   case S.indexOf prefix str of
     Just 0 -> Just $ S.drop (S.length prefix) str
     _      -> Nothing
+
+newtype PursuitResult = PursuitResult
+  { text :: String
+  , package :: String
+  }
+
+instance isForeignPursuitResult :: IsForeign PursuitResult where
+  read o =
+    map PursuitResult $
+      { text: _, package: _ }
+        <$> readProp "text" o
+        <*> readProp "package" o
+
+renderResult :: PursuitResult -> String
+renderResult (PursuitResult r) = r.text <> " (" <> r.package <> ")"
